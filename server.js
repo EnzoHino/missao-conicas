fd90bb1333d9af5468766605051c71ed0f276b81
EnzoHino <enzoyudihino@gmail.com>
2026-06-11 14:36:33 -0300

    fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const path = require('path');
+const path    = require('path');
 
-const app = express();
+const app  = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Missão Cônicas rodando na porta ${PORT}`);
+  console.log(`Engenheiro Espacial rodando em http://localhost:${PORT}`);
 });
